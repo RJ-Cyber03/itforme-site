8f505f8a57f02240ee291d0a5ddcc8b4d080a498
RJ-Cyber03 <rossgrant.cyber@gmail.com>
2025-07-07 23:05:57 +0100

    Add index.html and main.jsx for proper build

---
+++ itforme-site/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ITforME from "./ITforME";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ITforME />
+  </React.StrictMode>
+);
